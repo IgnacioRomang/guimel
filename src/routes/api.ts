f0ffd2c6e7ec3fe450ccbd5326f31d2fd6f7e544
IgnacioRomang <IgnacioRomang@outlook.com>
2024-09-10 11:11:10 -0300

    clear main branch

--- src/routes/api.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-
-const routersApi = express.Router();
-
-routersApi.use("/", () => {});
-
-export default routersApi;
--- src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-
-const routers = express.Router();
-
-routers.use("/api", routers);
-
-export default routers;
